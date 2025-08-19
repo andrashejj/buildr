15720b6624cd248818d0360f6213fa0d5d8a9137
Andras Hejj <andras@pierwallet.com>
2025-08-19 10:24:05 +0200

    Update configuration and dependencies for Expo and RoomPlan integration

--- ui/app/index.tsx
+++ ui/app/index.tsx
@@ -1,12 +1,17 @@
-import { Text, View } from "react-native";
+import { ExportType, useRoomPlan } from 'expo-roomplan';
+import { Text, View } from 'react-native';
 
 export default function Index() {
+  const { startRoomPlan } = useRoomPlan({
+    exportType: ExportType.Parametric,
+    sendFileLoc: false,
+  });
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
